6f0d84319b57043e81ff1cc05fa6a77dab34a57d
junayed-code <junayed.code@gmail.com>
2023-10-19 23:52:11 +0600

    Section component is created

---
+++ src/components/Section.jsx
@@ -0,0 +1,31 @@
+export default function Section({ className, children }) {
+  return <section className={className}>{children}</section>;
+}
+
+// Section heading component
+Section.Heading = ({ className = "", children }) => (
+  <h1
+    className={"text-4xl sm:text-6xl font-bold mb-5 ".concat(className).trim()}
+  >
+    {children}
+  </h1>
+);
+
+// Section title component
+Section.Title = ({ className = "", children }) => (
+  <h2 className={"text-3xl font-bold mb-5 ".concat(className).trim()}>
+    {children}
+  </h2>
+);
+
+// Section description component
+Section.Description = ({ className = "", children }) => (
+  <p className={"text-base-content/75 sm:text-lg ".concat(className).trim()}>
+    {children}
+  </p>
+);
+
+// Section content component
+Section.Content = ({ className, children }) => (
+  <div className={className}>{children}</div>
+);
