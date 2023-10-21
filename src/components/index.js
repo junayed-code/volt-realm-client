7dddc92b40fc4be2a939571aabd8098344f0a549
junayed-code <junayed.code@gmail.com>
2023-10-21 23:02:31 +0600

    App components is uptodate

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+export { default as Card } from "./Card";
+export { default as Navbar } from "./Navbar";
+export { default as Button } from "./Button";
+export { default as Header } from "./Header";
+export { default as Footer } from "./Footer";
+export { default as Section } from "./Section";
+export { default as Loading } from "./Loading";
+export { default as Container } from "./Container";
+export { default as StarRating } from "./StarRating";
+export { default as ProductCard } from "./ProductCard";
