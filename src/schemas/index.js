28c7e46470bff3de30062e82952d3eabd094c566
junayed-code <junayed.code@gmail.com>
2023-10-21 22:45:02 +0600

    Add product form and update product form schemas is added

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,6 +1,6 @@
 import * as Yup from "yup";
 
-export const registerSchema = Yup.object().shape({
+export const signUpSchema = Yup.object().shape({
   uname: Yup.string()
     .min(2, "User name must be at least 2 characters")
     .max(25, "User name must be at most 25 characters")
@@ -16,10 +16,40 @@ export const registerSchema = Yup.object().shape({
       "Must include at least 1 special character - [#?!@$%^&*-]"
     )
     .required("This field is required."),
-  photoURL: Yup.string().url(),
 });
 
 export const logInSchema = Yup.object().shape({
   email: Yup.string().email().required("This field is required."),
   password: Yup.string().min(6).required("This field is required."),
+});
+
+export const addProductSchema = Yup.object().shape({
+  name: Yup.string().min(5).max(50).required("This field is required."),
+  brand: Yup.string().min(3).max(30).required("This field is required."),
+  type: Yup.string().min(4).max(25).required("This field is required."),
+  image: Yup.string().url().required("This field is required."),
+  price: Yup.number("Price must be a 'number' type")
+    .positive("Price must be a positive number")
+    .required("This field is required."),
+  rating: Yup.number("Rating must be a 'number' type")
+    .positive("Rating must be a positive number")
+    .required("This field is required."),
+  description: Yup.string()
+    .min(20)
+    .max(120)
+    .required("This field is required."),
+});
+
+export const updateProductSchema = Yup.object().shape({
+  name: Yup.string().min(5).max(50),
+  brand: Yup.string().min(3).max(30),
+  type: Yup.string().min(4).max(25),
+  image: Yup.string().url(),
+  price: Yup.number("Price must be a 'number' type value").positive(
+    "Price must be a positive number"
+  ),
+  rating: Yup.number("Rating must be a 'number' type value").positive(
+    "Rating must be a positive number"
+  ),
+  description: Yup.string().min(20).max(120),
 });
