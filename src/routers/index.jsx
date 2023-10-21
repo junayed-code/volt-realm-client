2f0a5c1933e12912a48b664788808efa1ac60f29
junayed-code <junayed.code@gmail.com>
2023-10-21 22:58:04 +0600

    Update app routes

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,9 +1,80 @@
 import { createBrowserRouter } from "react-router-dom";
+import { Main } from "../layouts";
+import {
+  AddProduct,
+  ErrorPage,
+  Home,
+  LogIn,
+  SignUp,
+  UpdateProduct,
+  ProductsOfBrand,
+  AllProducts,
+  ProductDetails,
+} from "../pages";
+import { productLoader } from "../pages/ProductDetails";
+import PrivateRoutes from "../routers/PrivateRoutes";
+
+const API_END_POINT = "https://volt-realm-api.vercel.app/products";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <button className="btn btn-secondary text-lg">Click Me</button>,
+    element: <Main />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+      {
+        path: "/login",
+        element: <LogIn />,
+      },
+      {
+        path: "all-products",
+        element: <AllProducts />,
+        loader: () => fetch(API_END_POINT),
+      },
+      {
+        path: "/products/brand/:name",
+        element: <ProductsOfBrand />,
+        loader: async ({ params }) => {
+          const res = await fetch(`${API_END_POINT}?brand=${params.name}`);
+          const resObject = await res.json();
+          if (resObject.error) {
+            throw new Response(null, {
+              status: resObject.error?.status,
+              statusText: resObject.error?.message,
+            });
+          }
+          return resObject.data;
+        },
+      },
+      {
+        path: "/",
+        element: <PrivateRoutes />,
+        children: [
+          {
+            path: "/add-product",
+            element: <AddProduct />,
+          },
+          {
+            path: "/products/update/:name",
+            element: <UpdateProduct />,
+            loader: productLoader,
+          },
+          {
+            path: "/products/:name",
+            element: <ProductDetails />,
+            loader: productLoader,
+          },
+        ],
+      },
+    ],
   },
 ]);
 
