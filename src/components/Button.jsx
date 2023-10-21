528d231ab9beb928679a673d0c1402045b3de177
junayed-code <junayed.code@gmail.com>
2023-10-21 12:45:07 +0600

    Update Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,26 @@
+import { Link } from "react-router-dom";
+
 export default function Button({
   type = "button",
   className = "",
   onClick,
   children,
+  to,
   ...rest
 }) {
+  if (to) {
+    return (
+      <Link
+        to={to}
+        className={"btn min-h-0 h-auto px-[1em] py-[0.5em] "
+          .concat(className)
+          .trim()}
+      >
+        {children}
+      </Link>
+    );
+  }
+
   return (
     <button
       {...rest}
