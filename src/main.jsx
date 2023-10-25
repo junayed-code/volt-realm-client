f28596e54cc9f77173385979e7a208b6fd933470
junayed-code <junayed.code@gmail.com>
2023-10-25 15:25:26 +0600

    Use QueryClientProvider and CartStorageProvider.

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import router from "./routers";
 import { RouterProvider } from "react-router-dom";
-import AuthProvider from "./providers/AuthProvider";
+import { AuthProvider, CartStorageProvider } from "./providers";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <CartStorageProvider>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </CartStorageProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
